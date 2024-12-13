c5fb8a00282e8c6308ebfe66d5f2d087606de17c
pdzabaleta <pdzabaleta@gmail.com>
2024-12-13 00:15:47 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'src',
-  build: {
-    outDir: '../dist',
-  },
+  root: 'src', // Directorio raíz (ajusta según tu estructura)
   server: {
-    open: '/pages/index.html',
+    port: 5173, // Puerto del servidor de desarrollo
+    open: true, // Abrir el navegador automáticamente
+  },
+  build: {
+    outDir: '../dist', // Carpeta de salida para el build
+    rollupOptions: {
+      input: {
+        main: './pages/index.html', // Archivo de entrada principal
+      },
+    },
   },
 });
